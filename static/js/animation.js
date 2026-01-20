75c7b38700373e885d85bbcc1a81ed1ffa52940e
Kartikey-0 <kartikeysaxena107@gmail.com>
2026-01-20 15:52:22 +0530

    feat: add intro animation screen before scanner load

---
+++ static/js/animation.js
@@ -0,0 +1,9 @@
+// setTimeout(() => {
+//   window.location.href = "animation.html"; 
+//   // replace index.html with scanner page if different
+// }, 2800);
+
+setTimeout(() => {
+  window.location.href = "/dashboard";
+}, 2000);
+
